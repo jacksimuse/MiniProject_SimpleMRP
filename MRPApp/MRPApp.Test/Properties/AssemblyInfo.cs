ede837f1702afe77524f6c8c585c2ff341f54c30
jacksimuse <cjh930306@naver.com>
2021-06-24 17:28:56 +0900

    MRPApp 단위테스트로 사용성 확인

---
+++ MRPApp/MRPApp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MRPApp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MRPApp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e1d40e0-9e93-4101-ab80-673dcef22094")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MRPApp/MRPApp.Test/SettingsTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MRPApp.View.Setting;
+using System;
+using System.Linq;
+
+namespace MRPApp.Test
+{
+    [TestClass]
+    public class SettingsTest
+    {
+        // Db사이에 중복된 데이터 있는지 테스트
+        [TestMethod]
+        public void IsDuplicateTest()
+        {
+            var expectVal = true; // 예상값
+            var inputCode = "PC010001"; // DB상에 있는 값
+
+            var code = Logic.DataAccess.GetSettings().Where(d => d.BasicCode.Contains(inputCode)).FirstOrDefault();
+            var realVal = code != null ? true : false;
+
+            Assert.AreEqual(expectVal, realVal);
+        }
+
+        [TestMethod]
+        public void IsCodeSearchTest()
+        {
+            var expectVal = 2; // 예상값
+            var inputCode = "설비"; // DB상에 있는 값
+
+            var code = Logic.DataAccess.GetSettings().Where(d => d.BasicCode.Contains(inputCode)).Count();
+            var realVal = code != null ? true : false;
+
+            Assert.AreEqual(expectVal, realVal);
+        }
+    }
+}
