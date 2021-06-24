6f445f14adfbd9bd404ad560bee8c7c5ddff06a1
jacksimuse <cjh930306@naver.com>
2021-06-24 17:30:30 +0900

    세팅리스트에 사용될 List추가

--- MRPApp/MRPApp/Logic/DataAccess.cs
+++ MRPApp/MRPApp/Logic/DataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MRPApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
@@ -9,6 +10,34 @@ namespace MRPApp.Logic
 {
     public class DataAccess
     {
-        
+        // 세팅테이블에서 데이터 가져오기
+        public static List<Settings> GetSettings()
+        {
+            List<Model.Settings> settings;
+
+            using (var ctx = new MRPEntities()) 
+                settings = ctx.Settings.ToList();
+
+            return settings;
+        }
+
+        internal static int SetSettings(Settings item)
+        {
+            using (var ctx = new MRPEntities())
+            {
+                ctx.Settings.AddOrUpdate(item);
+                return ctx.SaveChanges(); // COMMIT
+            }
+        }
+
+        internal static int Delsetting(Settings item)
+        {
+            using (var ctx = new MRPEntities())
+            {
+                var obj = ctx.Settings.Find(item.BasicCode);
+                ctx.Settings.Remove(obj); // DELETE
+                return ctx.SaveChanges();
+            }
+        }
     }
 }
