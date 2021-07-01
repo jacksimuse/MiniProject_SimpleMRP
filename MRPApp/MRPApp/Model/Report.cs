607d84ac7032e4060f91c0dfadc1575d80780793
jacksimuse <cjh930306@naver.com>
2021-07-01 16:58:20 +0900

    리포트를 위한 가상 테이블 클래스

---
+++ MRPApp/MRPApp/Model/Report.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRPApp.Model
+{
+    /// <summary>
+    /// 리포트를 위한 가상 테이블 클래스 생성
+    /// </summary>
+    public class Report
+    {
+        public int SchIdx { get; set; }
+        public string PlantCode { get; set; }
+        public Nullable<int> SchAmount { get; set; }
+        public System.DateTime PrcDate { get; set; }
+        public Nullable<int> PrcOKAmount { get; set; }
+        public Nullable<int> PrcFailAmount { get; set; }
+    }
+}
