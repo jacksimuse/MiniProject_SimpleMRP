2e1b9a211013a04eadf431d2bf47cce7073bab40
jacksimuse <cjh930306@naver.com>
2021-07-01 16:57:15 +0900

    DB에서 JOIN기능을 통해 가상 테이블 만든 후 REPORT의 차트에 연결

--- MRPApp/MRPApp/Logic/DataAccess.cs
+++ MRPApp/MRPApp/Logic/DataAccess.cs
@@ -1,7 +1,9 @@
 ﻿using MRPApp.Model;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Entity.Migrations;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -76,6 +78,79 @@ namespace MRPApp.Logic
                 ctx.Process.AddOrUpdate(item); // INSERT | UPDATE
                 return ctx.SaveChanges(); // COMMIT
             }
+        }
+
+        internal static List<Report> GetReportDatas(string startDate, string endDate, string plantCODE)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["MRPConnString"].ToString();
+            var list = new List<Report>();
+            var lastObj = new Model.Report(); // 추가 : 최종 Report값 담는 변수
+
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();    // 중요!!
+                var sqlQuery = $@"select sch.SchIdx, sch.PlantCode, sch.SchAmount, PrcDate, PrcOKAmount, PrcFailAmount
+                                    from Schedules Sch
+                                   inner join (
+                                  select smr.SchIdx, smr.PrcDate,
+	                                     sum(smr.prcok) PrcOKAmount, sum(smr.prcfail) PrcFailAmount
+                                    from (select p.SchIdx, p.PrcDate,
+			                                     case p.PrcResult WHEN 1 then 1 else 0 end PrcOK,
+			                                     case p.PrcResult when 0 then 1 else 0 end PrcFail
+		                                    from Process as p
+		                               ) smr
+                                   GROUP by smr.SchIdx, smr.PrcDate
+                                   ) prc
+                                      on sch.SchIdx = prc.SchIdx
+                                   where sch.PlantCode = '{plantCODE}'
+                                     and prc.PrcDate between '{startDate}' and '{endDate}'";
+
+                var cmd = new SqlCommand(sqlQuery, conn);
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var tmp = new Report
+                    {
+                        SchIdx = (int)reader["SchIdx"],
+                        PlantCode = reader["PlantCode"].ToString(),
+                        PrcDate = DateTime.Parse(reader["PrcDate"].ToString()),
+                        SchAmount = (int)reader["SchAmount"],
+                        PrcOKAmount = (int)reader["PrcOKAmount"],
+                        PrcFailAmount = (int)reader["PrcFailAmount"],
+                    };
+                    list.Add(tmp);
+                    lastObj = tmp; // 마지막 값을 할당
+                }
+
+                // 시작일부터 종료일까지 없는 값 만들어주는 로직
+                var DtStart = DateTime.Parse(startDate);
+                var DtEnd = DateTime.Parse(endDate);
+                var DtCurrent = DtStart;
+
+                while (DtCurrent < DtEnd)
+                {
+                    var count = list.Where(c => c.PrcDate.Equals(DtCurrent)).Count();
+                    if (count == 0)
+                    {
+                        // 새로운 Report(없는 날짜)
+                        var tmp = new Report
+                        {
+                            SchIdx = lastObj.SchIdx,
+                            PlantCode = lastObj.PlantCode,
+                            PrcDate = DtCurrent,
+                            SchAmount = 0,
+                            PrcOKAmount = 0,
+                            PrcFailAmount = 0
+                        };
+                        list.Add(tmp);
+                    }
+                    DtCurrent = DtCurrent.AddDays(1); // 날하루 증가
+                }
+            }
+
+            list.Sort((reportA, reportB) => reportA.PrcDate.CompareTo(reportB.PrcDate)); // 가장오래된 날짜부터 오름차순 정령
+            return list;
         }
     }
 }
