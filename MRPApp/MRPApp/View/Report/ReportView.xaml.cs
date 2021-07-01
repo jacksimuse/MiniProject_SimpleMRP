f25a8250766fa722498b1757e74eceafdb5807ac
jacksimuse <cjh930306@naver.com>
2021-07-01 17:07:17 +0900

    차트 기능 수정 및 목록 Linq와 연결

--- MRPApp/MRPApp/View/Report/ReportView.xaml.cs
+++ MRPApp/MRPApp/View/Report/ReportView.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace MRPApp.View.Report
 {
@@ -19,6 +22,8 @@ namespace MRPApp.View.Report
             try
             {
                 InitControls();
+
+                //DisplayChart();
             }
             catch (Exception ex)
             {
@@ -27,31 +32,76 @@ namespace MRPApp.View.Report
             }
         }
 
+        private void DisplayChart(List<Model.Report> list)
+        {
+            int[] schAmount = list.Select(a => (int)a.SchAmount).ToArray();
+            int[] prcOKAmounts = list.Select(a => (int)a.PrcOKAmount).ToArray();
+            int[] prcFailAmounts = list.Select(a => (int)a.PrcFailAmount).ToArray();
+
+            var series1 = new LiveCharts.Wpf.ColumnSeries
+            {
+                Title = "계획수량",
+                Fill = new SolidColorBrush(Colors.Green), 
+                Values = new LiveCharts.ChartValues<int>(schAmount)
+            };
+            var series2 = new LiveCharts.Wpf.ColumnSeries
+            {
+                Title = "성공수량",
+                Fill = new SolidColorBrush(Colors.Blue),
+
+                Values = new LiveCharts.ChartValues<int>(prcOKAmounts)
+            };
+            var series3 = new LiveCharts.Wpf.ColumnSeries
+            {
+                Title = "실패수량",
+                Fill = new SolidColorBrush(Colors.Red),
+
+                Values = new LiveCharts.ChartValues<int>(prcFailAmounts)
+            };
+
+            // 차트할당
+            ChtReport.Series.Clear();
+            ChtReport.Series.Add(series1);
+            ChtReport.Series.Add(series2);
+            ChtReport.Series.Add(series3);
+            // x축 좌표값을 날짜로 표시
+            ChtReport.AxisX.First().Labels = list.Select(a => a.PrcDate.ToString("yyyy-MM-dd")).ToList();
+        }
+
         private void InitControls()
         {
             DtpSearchStartDate.SelectedDate = DateTime.Now.AddDays(-7);
             DtpSearchDate.SelectedDate = DateTime.Now;
         }
 
-        private void BtnEditMyAccount_Click(object sender, RoutedEventArgs e)
-        {
-           // NavigationService.Navigate(new EditAccount()); // 계정정보 수정 화면으로 변경
-        }
-
         private void BtnSearch_Click(object sender, RoutedEventArgs e)
         {
             if (IsValidInputs())
             {
-                MessageBox.Show("검색시작!!");
+                var startDate = ((DateTime)DtpSearchStartDate.SelectedDate).ToString("yyyy-MM-dd");
+                var endDate = ((DateTime)DtpSearchDate.SelectedDate).ToString("yyyy-MM-dd");
+                var searchResult = Logic.DataAccess.GetReportDatas(startDate, endDate, Commons.PLANTCODE);
+                DisplayChart(searchResult);
             }
         }
 
+        // 날짜가 빠져있거나, StartDate가 EndDate보다 최신이면 검색하면 안됨
         private bool IsValidInputs()
         {
             var result = true;
 
-            // TODO 검증
-            
+            if (DtpSearchStartDate.SelectedDate == null || DtpSearchDate.SelectedDate == null)
+            {
+                Commons.ShowMessageAsync("검색", "검색 할 일자를 선택하세요.");
+                result = false;
+            }
+
+            if (DtpSearchStartDate.SelectedDate > DtpSearchDate.SelectedDate)
+            {
+                Commons.ShowMessageAsync("검색", "시작 일자가 종료 일자보다 최신 일 수 없습니다.");
+                result = false;
+            }
+
             return result;
         }
     }
